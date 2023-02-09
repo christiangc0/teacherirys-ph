d579f9856aacaf2e4cd16560d00f0a8c298dae37
christiangc0 <124887122+christiangc0@users.noreply.github.com>
2023-02-09 20:08:56 +0800

    Add files via upload

---
+++ root/index.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>ABI ICT Helpesk</title>
+<style type="text/css">
+	body { margin: 0px; font-family: "Lucida Grande", "Lucida Sans Unicode", "Lucida Sans", "DejaVu Sans", Verdana, "sans-serif"; font-size: 14px;}
+	#pageTop {
+		background: url(style/Sliver.png);
+		height: 90px;
+	}
+	#pageTop > #pageTopWrap {
+		width: 1000px;
+		margin: 0px auto;
+		height: 90px;
+	}
+	#pageTop > #pageTopWrap > #pageTopLogo {
+		float: left;
+		height: 90px;
+		width: 108px;
+	}
+	#pageTop > #pageTopWrap > #pageTopRest {
+		float: left;
+		height: 90px;
+		width: 892px;
+	}
+	#pageTop > #pageTopWrap > #pageTopRest > #About {
+		height: 45px;
+	}
+	#pageTop > #pageTopWrap > #pageTopRest > #Services {
+		height: 20px;
+	}
+	#pageTop > #pageTopWrap > #pageTopRest > #Services > div {
+		margin-top: 10px;
+		padding: 4px;
+	}
+	#pageTop > #pageTopWrap > #pageTopRest > #Services > div > a {
+		display: block;
+		float: left;
+		color: #CCC;
+		text-decoration: none;
+		margin: 0px 12px;
+	}
+	#pageTop > #pageTopWrap > #pageTopRest > #Services > div > a:hover {
+		color: aqua;
+	
+	}
+</style>
+</head>
+<body>
+	
+<div id="pageTop">
+	<div id="pageTopWrap">
+		<div id="pageTopLogo">
+			<a href="https://www.google.com"><img src="images/logo.png" alt="Logo" title="Teacher Irys 1.0"></a>
+		</div>
+		<div id="pageTopRest">
+			<div id="About">
+				<div>
+				<a href="#">Sign Up / Log In</a>
+			</div>
+			</div>
+			<div id="Services">	
+			<div>
+				<a href="https://www.google.com"><img src="images/home.png" alt="Home" title="Teacher Irys Home"></a>
+				<a href="#">About</a>
+				<a href="#">Services</a>
+			</div>
+			</div>
+	</div>	
+</div>	
+	
+<div id="pageMiddle"></div>
+<div id="pageBottom"></div>
+</body>
+</html>
